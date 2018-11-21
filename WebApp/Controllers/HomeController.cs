6233c9eb9271e747a473ab1cc0ec78634d4a549b
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-11-21 11:29:39 +0100

    Config Stuff

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private IHostingEnvironment _environment;
+        private readonly IConfiguration _config;
+
+        public HomeController(IHostingEnvironment environment, IConfiguration config)
+        {
+            _environment = environment;
+            _config = config;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = new HomeIndexViewModel();
+            model.EnvironmentInfo = _environment.EnvironmentName;
+
+            model.MyKey = _config.GetValue<string>("MyKey");
+            model.EmailUsername = _config.GetValue<string>("Email:Username"); // Gå in i sektion Email, hämta Username
+
+            if (_environment.IsDevelopment())
+            {
+                // Gör någon debug i mijön Development grej...
+                model.EnvironmentInfo = model.EnvironmentInfo+ " Gör en Debug grej... ";
+            }
+
+            if (_environment.IsEnvironment("Integration"))
+            {
+                // Gör någon debug i miljön Environment grej...
+                model.EnvironmentInfo = model.EnvironmentInfo + " Gör en specialare i Integration... ";
+            }
+
+
+            return View(model);
         }
 
         public IActionResult About()
---
+++ WebApp/Models/HomeIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class HomeIndexViewModel
+    {
+        public string EnvironmentInfo { get; set; }
+
+        public string MyKey { get; set; }
+        public string EmailUsername { get; set; }
+    }
+}
